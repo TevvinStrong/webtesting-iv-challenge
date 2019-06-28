99d806c9191ce89358b6f6ffba6e2de49dac5d97
Tevvin <Tevvin7395@icloud.com>
2019-06-27 22:12:17 -0400

    Installed nodemon, added index,js/server.js server is up an running

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+//const carsRouter = require('../cars/cars-router.js');
+
+const server = express();
+
+server.use(express.json());
+
+//test request
+server.get('/', (req, res) => {
+    res.send('Server is working');
+});
+
+module.exports = server;
